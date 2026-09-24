cb0cd7603652531c0ea1e9791f0e416f5e822fc3
RobitOverlord <193709524+10-X-eng@users.noreply.github.com>
2026-09-23 23:25:31 -0500

    Accept spaces in native Windows file paths

--- packaging/windows/test/Test-Windows.C
+++ packaging/windows/test/Test-Windows.C
@@ -44,6 +44,9 @@ int main(int argc, char** argv)
     if (fileName("C:/case").path() != fileName("C:/")) return 13;
     if (fileName("C:/").path() != fileName("C:/")) return 14;
     if (fileName("relative/case").path() != fileName("relative")) return 15;
+    if (fileName("C:\\case folder\\system") != fileName("C:/case folder/system")) return 16;
+    if (!fileName::valid(' ') || fileName::valid('\n')) return 17;
+    if (fileName("C:/case folder/system").path() != "C:/case folder") return 18;
     if (mag(int64_t(-4294967296LL)) != int64_t(4294967296LL)) return 5;
     const void* low = reinterpret_cast<void*>(std::uintptr_t(1));
     const void* high = reinterpret_cast<void*>(std::uintptr_t(0x100000001ULL));
